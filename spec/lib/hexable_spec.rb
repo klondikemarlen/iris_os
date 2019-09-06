920acaf66774272944dda0ed81ddcd806b22376a
Marlen Brunner <klondikemarlen@gmail.com>
2019-09-06 11:17:23 -0700

    Fix bug where DummyClass was being re-opened by all specs.
    I noticed that some methods that shouldn't have existed did. I corrected this by using Class.new in each spec.
    
    I'm still a little confused about why I don't need to import all the other modules.

--- spec/lib/hexable_spec.rb
+++ spec/lib/hexable_spec.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-class DummyClass
-  include Hexable
-end
+WithHexable = Class.new { include Hexable }
 
 describe Hexable do
-  subject(:dc) { DummyClass.new }
+  subject(:with_hexable) { WithHexable.new }
   context '#to_hex' do
     it 'converts decimals' do
-      expect(dc.to_hex(1234)).to eq 'd204'
+      expect(with_hexable.to_hex(1234)).to eq 'd204'
     end
 
     it 'converts hex' do
-      expect(dc.to_hex(0x234)).to eq '3402'
+      expect(with_hexable.to_hex(0x234)).to eq '3402'
     end
 
     it 'converts upper case letters' do
-      expect(dc.to_hex('H')).to eq '48'
+      expect(with_hexable.to_hex('H')).to eq '48'
     end
 
     it 'converts lower case letters' do
-      expect(dc.to_hex('e')).to eq '65'
+      expect(with_hexable.to_hex('e')).to eq '65'
     end
 
     it 'raises an error if you pass it a string of hex' do
-      expect { dc.to_hex('e9') }.to raise_error ArgumentError
+      expect { with_hexable.to_hex('e9') }.to raise_error ArgumentError
     end
   end
 end
--- spec/lib/jumps_spec.rb
+++ spec/lib/jumps_spec.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-class DummyClass
+WithJumps = Class.new do
+  include BaseAsm
+  include Hexable
   include Jumps
 end
 
 describe Jumps do
-  subject(:dc) { DummyClass.new }
+  subject(:with_jumps) { WithJumps.new }
   context '#jmp' do
     it 'can understand jmp to label' do
-      dc.label :loop do
+      with_jumps.label :loop do
         jmp :loop
       end
 
-      expect(dc.buffer).to eq 'e9fdff'
+      expect(with_jumps.buffer).to eq 'e9fdff'
     end
 
     it 'can jump to the current address (i.e. forever)' do
-      expect(dc.jmp('$').join).to eq 'e9fdff'
+      expect(with_jumps.jmp('$').join).to eq 'e9fdff'
     end
   end
 end
