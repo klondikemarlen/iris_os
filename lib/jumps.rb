5ca3afbdfdcaaa11a39dc4335f3bfdae8f8b5247
Marlen Brunner <klondikemarlen@gmail.com>
2019-09-13 19:07:08 -0700

    Add information on NASM jumps.

--- lib/jumps.rb
+++ lib/jumps.rb
@@ -3,6 +3,12 @@
 module Jumps
   attr_reader :target
 
+  # NASM supports two special tokens in expressions, allowing calculations to
+  # involve the current assembly position: the $ and $$ tokens. $ evaluates to
+  # the assembly position at the beginning of the line containing
+  # the expression; so you can code an infinite loop using JMP $. $$ evaluates
+  # to the beginning of the current section; so you can tell how far into the
+  # section you are by using ($-$$).
   # jmp '$'   ; Jump to the current address (i.e. forever).
   def address
     case target
