fad54c02a59789da51ca5555360e47fe4a687561
Marlen Brunner <klondikemarlen@gmail.com>
2019-09-15 20:41:54 -0700

    Push type casting of ordinals into Immediate.

--- lib/immediate.rb
+++ lib/immediate.rb
@@ -9,10 +9,35 @@ class Immediate
   attr_reader :value
 
   def initialize(value)
-    @value = value
+    if value.is_a? Integer
+      @value = value
+      return
+    end
+
+    @value = cast_to_ord(value)
   end
 
   def width
-    to_hex(value).length * 4
+    to_s.length * 4
+  end
+
+  def to_s
+    to_hex(value)
+  end
+
+  #######
+  private
+  #######
+
+  def cast_to_ord(char)
+    unless char.is_a? String
+      raise ArgumentError, 'Can only cast String to ordinal.'
+    end
+
+    unless char.length == 1
+      raise ArgumentError, 'Only single characters allowed.'
+    end
+
+    char.ord
   end
 end
--- spec/lib/immediate_spec.rb
+++ spec/lib/immediate_spec.rb
@@ -4,17 +4,45 @@ require 'immediate'
 
 describe Immediate do
   subject(:immediate) { described_class.new value }
-  context 'an 8-bit, byte immediate' do
-    let(:value) { 0x0e }
-    it 'has a width of 8' do
-      expect(immediate.width).to eql 8
+
+  describe '#new' do
+    context 'when passed a character' do
+      let(:value) { 'H' }
+      it 'converts to its ASCII ordinal' do
+        expect(immediate.to_s).to eql '48'
+      end
+    end
+
+    context 'when passed a many character string' do
+      let(:value) { 'Long String' }
+      it 'fails informatively' do
+        expect { immediate }.to raise_error ArgumentError,
+                                            /Only single characters allowed/
+      end
+    end
+
+    context 'when passed a float (or other thing)' do
+      let(:value) { 3.7 }
+      it 'fails informatively' do
+        expect { immediate }.to raise_error ArgumentError,
+                                            /Can only cast String to ordinal/
+      end
     end
   end
 
-  context 'a 16-bit, word immediate' do
-    let(:value) { 0xaa55 }
-    it 'has a width of 16' do
-      expect(immediate.width).to eql 16
+  describe '#width' do
+    context 'an 8-bit, byte immediate' do
+      let(:value) { 0x0e }
+      it 'has a width of 8' do
+        expect(immediate.width).to eql 8
+      end
+    end
+
+    context 'a 16-bit, word immediate' do
+      let(:value) { 0xaa55 }
+      it 'has a width of 16' do
+        expect(immediate.width).to eql 16
+      end
     end
   end
 end
