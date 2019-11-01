03745741e07ac6fa14b81e92a15c8ceaec275a56
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-31 19:20:53 -0700

    Add failing test for 3rd boot sector implementation.

--- spec/boot_sector_spec.rb
+++ spec/boot_sector_spec.rb
@@ -6,13 +6,14 @@ describe 'boot sector' do
   context 'when run on v1 code' do
     let(:boot_sect) do
       Asm.build do
-        label :loop do
-          jmp :loop
+        label :endless do
+          jmp endless
         end
         pad 510, 0
         dw 0xaa55
       end
     end
+
     it 'is correct' do
       expect(boot_sect.hexdump).to eql <<~HEX.chomp
         e9 fd ff 00 00 00 00 00 00 00 00 00 00 00 00 00
@@ -22,6 +23,7 @@ describe 'boot sector' do
       HEX
     end
   end
+
   context 'when run on v2 code' do
     let(:boot_sect) do
       Asm.build do
@@ -41,10 +43,56 @@ describe 'boot sector' do
         dw 0xaa55
       end
     end
+
     it 'produces the correct hex data' do
       expect(boot_sect.hexdump).to eql <<~HEX.chomp
         b4 0e b0 48 cd 10 b0 65 cd 10 b0 6c cd 10 b0 6c
         cd 10 b0 6f cd 10 e9 fd ff 00 00 00 00 00 00 00
+        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+        *
+        00 00 00 00 00 00 00 00 00 00 00 00 00 00 55 aa
+      HEX
+    end
+  end
+
+  context 'when run on v3 code' do
+    let(:boot_sect) do
+      Asm.build do
+        label :the_secret do
+          db 'X'
+        end
+
+        mov ah, 0x0e # scrolling teletype BIOS routine
+
+        # first attempt
+        mov al, the_secret
+        int 0x10
+
+        # second attempt
+        mov al, [the_secret]
+        int 0x10
+
+        # third attempt
+        mov bx, the_secret
+        add bx, 0x7c00
+        mov al, [bx]
+        int 0x10
+
+        # fourth attempt
+        mov al, [0x7c1e]
+        int 0x10
+
+        jmp '$' # jump forever
+
+        # padding and magic boot block BIOS id number
+        pad 510, 0
+        dw 0xaa55
+      end
+    end
+    it 'produces the correct output' do
+      expect(boot_sect.hexdump).to eql <<~HEX.chomp
+        b4 0e b0 1e cd 10 a0 1e 00 cd 10 bb 1e 00 81 c3
+        00 7c 8a 07 cd 10 a0 1e 7c cd 10 e9 fd ff 58 00
         00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
         *
         00 00 00 00 00 00 00 00 00 00 00 00 00 00 55 aa
